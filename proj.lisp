96c8b8d126b91029e8ccce7a67c9ee717c0fca4a
marezi <markoilic22122012@gmail.com>
2019-11-22 11:39:48 +0100

    Start

---
+++ proj.lisp
@@ -0,0 +1,27 @@
+(defstruct byte
+    matrix
+    matSize
+)
+
+(defun byteGame ()
+    (setq n (read))
+    (setq byte-matSize n)
+    (if (eq (mod byte-matSize '2) '0) (draw byte-matSize) (byteGame)
+    )
+)
+
+(defun draw (matSize)
+  (cond 
+      ((>= matSize 0) (draw (- matSize 1)) (format t "~a            " matSize))
+  )
+)
+
+(defun stampaj (stanje)
+(format t "~%~a ~a ~a~%" (nth 0 stanje)
+(nth 1 stanje) (nth 2 stanje))
+(format t "~a ~a ~a~%" (nth 3 stanje)
+(nth 4 stanje) (nth 5 stanje))
+(format t "~a ~a ~a~%" (nth 6 stanje)
+(nth 7 stanje) (nth 8 stanje)))
+
+(byteGame)
